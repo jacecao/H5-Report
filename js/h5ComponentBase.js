038889866df9b6665ce1a14e6c85012269a13b99
jacecao <280749498@qq.com>
2016-09-04 02:52:17 +0800

    加入散点图组件和测试该组件

--- js/h5ComponentBase.js
+++ js/h5ComponentBase.js
@@ -2,7 +2,8 @@
 
 var H5ComponentBase = function( setClass, cfg ) {
 
-  if( arguments.length === 1 ) {
+  if( cfg === undefined || arguments.length === 1 ) {
+
     switch (typeof arguments[0]) {
       case 'object':
         cfg = arguments[0];
@@ -12,36 +13,37 @@ var H5ComponentBase = function( setClass, cfg ) {
         cfg = {};
         break; 
     }
+    
   }
 
-  //设置默认配置文件
+//设置默认配置文件
   var config = $.extend({
     
   }, cfg);
 
-  //取得一个随机数来作为id取值，这里保留小数点后3位
+//取得一个随机数来作为id取值，这里保留小数点后3位
   var _id = Math.round( ( Math.random()*1000 ) ) / 1000;
   //替换小数点位下划线
   var id = ('h5_base' + _id).replace('.','_'); 
 
-  //设置自定义的class
+//设置自定义的class
   var _className = setClass ? setClass : '';
   var component = $('<div class="h5_component h5_component_base '+ _className +'" id="'+ id +'"></div>');
   
-  //加入文本
+//加入文本
   config.text && component.text( config.text );
 
-  //加入样式
+//加入样式
   config.width && component.width( config.width/2 );
   config.height && component.height( config.height/2 );
   config.css && component.css( config.css );
   
-  //加入图片背景
+//加入图片背景
   config.backgroundImage && component.css({
     'background-image': 'url('+ config.backgroundImage +')'
   });
 
-  //组件对齐方式
+//组件对齐方式
   switch( config.align ) {
     case 'verticalCenter':
       component.css({
@@ -59,10 +61,40 @@ var H5ComponentBase = function( setClass, cfg ) {
       break;  
   }
 
-  // 自定义事件
+//相对上一个组件定位 relativeTo
+  /*
+  var relativeTo = function() {
+    if( config.relativeTo && typeof config.relativeTo === 'object' ) {
+      
+      var prev_ele = $('.h5_component_base').last();
+      var _top,_left;
+
+      if( prev_ele[0] != undefined ) {
+        //获取上一个组件的top & left
+        _top = prev_ele.offset().top;
+        _left = prev_ele.offset().left;
+      }else{
+        _top = _left = 0;
+      }
+      
+      //设置当前组件的top & left
+      config.relativeTo.top && component.css('top', _top + config.relativeTo.top);
+      config.relativeTo.top && component.css('left', _left + config.relativeTo.left);
+
+    }else if( config.relativeTo && typeof config.relativeTo != 'object' ) {
+
+      console.error('relativeTo是一个配置对象，这里配置不正确');
+
+    }
+  };
+  */
+
+// 自定义事件
   var cls = 'h5_component_base_';
   component.on('onLoad', function(event) {
     
+    //relativeTo();
+
     event.stopPropagation();
     //这里加class是为了更自由的添加组件加载样式
     $(this).addClass(cls + 'load').removeClass(cls+'leave');
---
+++ js/h5ComponentPoint.js
@@ -0,0 +1,114 @@
+//散点图组件
+var H5ComponentPoint = function( setClass, cfg ) {
+  
+  //创建一个基本组件
+  var component = new H5ComponentBase( setClass, cfg );
+
+  //创建散点元素
+  //必须要有data配置项，且该项目里一定要有value属性
+  if( cfg.data && cfg.data[0].value) {
+
+    //将第一组数据中的值作为比例参考点
+    var base = cfg.data[0].value;
+
+    //寻找最大值并存储该对象索引
+    var maxObj = {max: null,index: null};
+    $.each( cfg.data, function(index,item){
+      if( item.value > maxObj.max ){
+        maxObj.max = item.value;
+        maxObj.index = index;
+      }
+    });
+
+    //设置point默认样式
+    var initCss = {
+      'position': 'absolute',
+      'border-radius': '50%'
+    };
+
+    //设置文字默认样式
+    var _fontSize = cfg.css.fontSize || '18px';
+    var textCss = {
+      width: '100%',
+      height: 30,
+      fontSize: _fontSize,
+      textAlign: 'center',
+      position: 'absolute',
+      top: '50%',
+      marginTop: -15
+    };
+
+    $.each( cfg.data, function(index, item ){
+      
+      var point = $('<div class="h5_component_point"></div>');
+      point.css(initCss);
+
+      //加入文字说明
+      var desc = $('<div class="point_text">'+ item.text +'</div>');
+      //百分比数据
+      var value_pre = $('<div class="point_pre">'+ (item.value*100) +'%</div>');
+      value_pre.css('fontSize', '0.5em');
+
+      desc.css(textCss).append(value_pre);
+      point.append(desc);
+
+      //根据参考值计算出相应的宽高比
+      var pre = (item.value/base)*100 + '%';
+      point.width( pre ).height( pre );
+      //设置背景色
+      item.color && point.css('backgroundColor', item.color);
+      
+      //设置相对component定位
+      if( index == maxObj.index ){
+        item.position && point.css( item.position ).css('zIndex','100');
+      }else{
+        //根据最大值的Point来定位其他Point
+        //这里需要大的完全覆盖小的
+        //所以 MAX.width/2+Max.top = min.top + min.width/2
+        //MAX.width = max*min.width/min(这里的图形本来就有一个比例参考)
+        //这里需要明白的就是如何推导出最大图形的宽度
+        var offsetLeft = ( maxObj.max*point.width() / (item.value*2) ) + cfg.data[maxObj.index].position.left - point.width()/2;
+        var offsetTop = ( maxObj.max*point.width() / (item.value*2) ) + cfg.data[maxObj.index].position.top - point.width()/2;
+        point.css({
+          'left': offsetLeft,
+          'top': offsetTop
+        }).css('zIndex','1');
+        //将定位数据储存到该元素中
+        point.data('position', {
+            _top: offsetTop, 
+            _left: offsetLeft, 
+            top: item.position.top,
+            left: item.position.left
+        });
+
+        //添加事件动画
+        point.on('onLoad', function(event){
+          event.stopPropagation();
+          $(this).animate({
+            top: $(this).data('position').top,
+            left: $(this).data('position').left
+          });
+        });
+
+        point.on('onLeave', function(event){
+          event.stopPropagation();
+          $(this).animate({
+            top: $(this).data('position')._top,
+            left: $(this).data('position')._left
+          });
+        });
+
+      }
+
+      component.append( point );
+
+    });
+
+  }else{
+    console.error('请加入数据，data:[{},....]');
+  }
+
+  
+
+  return component;
+};
