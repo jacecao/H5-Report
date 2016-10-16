54bbe4b774731f295f75643852d8850969f96e1d
jacecao <280749498@qq.com>
2016-10-16 23:02:29 +0800

    完成折线图数据描绘

--- js/h5ComponentPolyline.js
+++ js/h5ComponentPolyline.js
@@ -8,6 +8,8 @@ var H5ComponentPolyline = function( setClass, cfg ) {
   var w = cfg.width;
   var h = cfg.height;
   var canvas = document.createElement('canvas');
+  // 这里加2个像素是因为首尾线条会超出边界
+  // 所以这里需要加入网格像素的2倍
   canvas.width = w + 2;
   canvas.height = h + 2;
   // 设置canvasc样式
@@ -19,12 +21,14 @@ var H5ComponentPolyline = function( setClass, cfg ) {
     height: '100%'
   });
   var ctx = canvas.getContext('2d');
+  // 设置绘制文字的大小和字体
+  ctx.font = '20px Arial';
   // 画网格
   var drawLine = function() {
     var step = 10;
     ctx.beginPath();
+    // 这里需要在画布宽高加入2倍lineWidth像素
     ctx.lineWidth = 1;
-    // 再画布前后两处加入2倍lineWidth像素
     ctx.strokeStyle = '#aaa';
     // 画水平线
     for( var i = 0; i < step + 1; i++) {
@@ -39,11 +43,51 @@ var H5ComponentPolyline = function( setClass, cfg ) {
       ctx.moveTo(x, 0);
       ctx.lineTo(x, h);
     }
+    ctx.stroke();
+  };
 
+  // 画数据层
+  var drawData = function() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.fillStyle = '#ff8878';
+    ctx.strokeStyle = '#ff8878';
+    // 画出数据点
+    var x , y;
+    // 每个网格间距
+    var _w = w / (cfg.data.length + 1);
+    for( var i = 0; i < cfg.data.length; i++) {
+      // 让每个数据点都在中间的竖线上
+      // 所以这里需要多移动一个位置
+      x = _w * i + _w;
+      // 为什么这里需要这样计算
+      // 这里需要弄清楚是坐标移动的
+      y = h * (1 - cfg.data[i].value);
+      ctx.moveTo(x,y);
+      ctx.arc(x, y, 5, 0, 2*Math.PI);
+    }
     ctx.closePath();
+    ctx.fill();
+    
+    // 连接每个数据点
+    ctx.beginPath();
+    // 移动到第一个数据点
+    ctx.moveTo(_w, h * (1 - cfg.data[0].value));
+    for( var j = 0; j < cfg.data.length; j++) {
+      x = _w * j + _w;
+      y = h * (1 - cfg.data[j].value);
+      ctx.lineTo(x, y);
+      // 写入数据到指定位置
+      var text = cfg.data[j].value*100 + '%';
+      // 获取文字的宽度
+      var text_w = ctx.measureText(text).width;
+      // 绘制文字
+      ctx.fillText(text, x - (text_w / 2), y - 15);
+    }
     ctx.stroke();
   };
   drawLine();
+  drawData();
 
   component.append(canvas);
   //必须要有data配置项，且该项目里一定要有value属性
